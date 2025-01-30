bd0297774e0a68d7b4c0046529028e0a40063a82
kindsco <yjrianx@gmail.com>
2025-01-30 18:54:55 +0800

    Update portfolio with new files

--- data/slide6.js
+++ data/slide6.js
@@ -1,11 +1,3 @@
-(function(){var loadHandler=window['sl_{1DB1B7BE-2BB3-4816-A682-E401EB6D3BCF}'];loadHandler&&loadHandler(5, '<div id="spr0_1da55b"><div id="spr1_1da55b" class="kern slide"><img id="img5_1da55b" src="data/img1.png" width="2560px" height="1440px" alt="" style="left:0px;top:0px;"/><div id="svg2_1da55b" style="left:84.667px;top:98.162px;"><svg width="2391" height="2" viewBox="-0.5 -0.5 2391 2"><g fill="#333333" fill-opacity="0.2" stroke="none"><path transform="translate(0.5,0.5)" d="M0,-0.5 A0.5,0.5 0 0,0 -0.5,0 A0.5,0.5 0 0,0 0,0.5 Z M2388.667,0.5 A0.5,0.5 0 0,0 2389.167,0 A0.5,0.5 0 0,0 2388.667,-0.5 Z M0,-0.5 h2388.667 v1 h-2388.667 Z"/></g></svg></div></div><div id="spr2_1da55b" class="kern slide"><div id="spr3_1da55b"><img id="img0_1da55b" src="data/img9.png" width="2560" height="1440.141" alt="图片包含 图形用户界面\
+(function(){var loadHandler=window['sl_{72C97886-0EC7-4CA2-B3B3-F20FFDFFB1CE}'];loadHandler&&loadHandler(5, '<div id="spr0_d1eb42"><div id="spr1_d1eb42" class="kern slide"><img id="img1_d1eb42" src="data/img1.png" width="2560px" height="1440px" alt="" style="left:0px;top:0px;"/><div id="svg2_d1eb42" style="left:84.667px;top:98.162px;"><svg width="2391" height="2" viewBox="-0.5 -0.5 2391 2"><g fill="#333333" fill-opacity="0.2" stroke="none"><path transform="translate(0.5,0.5)" d="M0,-0.5 A0.5,0.5 0 0,0 -0.5,0 A0.5,0.5 0 0,0 0,0.5 Z M2388.667,0.5 A0.5,0.5 0 0,0 2389.167,0 A0.5,0.5 0 0,0 2388.667,-0.5 Z M0,-0.5 h2388.667 v1 h-2388.667 Z"/></g></svg></div></div><div id="spr2_d1eb42" class="kern slide"><div id="spr3_d1eb42"><img id="img0_d1eb42" src="data/img10.png" width="2560" height="1440.141" alt="图片包含 图形用户界面\
 \
-描述已自动生成" style="top:-0.053px;"/></div><div id="spr4_1da55b" style="left:1309.714px;top:126.239px;"><div id="spr5_1da55b"><img id="img1_1da55b" src="data/img10.jpg" width="1164.114" height="654.814" alt="山上的风景\
-\
-中度可信度描述已自动生成" style="left:-0.006px;top:-0.003px;"/></div><div id="spr6_1da55b" style="left:651.09px;top:960.024px;"><img id="img2_1da55b" src="data/img11.png" width="513.05" height="288.778" alt="黑暗中的手机\
-\
-低可信度描述已自动生成" style="left:-0.016px;top:-0.079px;"/></div><div id="spr7_1da55b" style="left:651.09px;top:663.131px;"><img id="img3_1da55b" src="data/img12.jpg" width="513.05" height="288.778" alt="图示, 工程绘图\
-\
-中度可信度描述已自动生成" style="left:-0.016px;top:-0.079px;"/></div><div id="spr8_1da55b" style="top:663.131px;"><img id="img4_1da55b" src="data/img13.gif" width="641.429" height="585.46" alt="电子键盘\
-\
-中度可信度描述已自动生成"/></div></div></div></div>', '{"s":[]}');})();
+描述已自动生成" style="top:-0.053px;"/></div><div id="vd0_d1eb42" style="left:1309.395px;top:789.5px;"></div></div></div>', '{"s":[]}');})();
